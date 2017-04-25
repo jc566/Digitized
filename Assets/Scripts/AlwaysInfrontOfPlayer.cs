5c4718cee0489f2ed25dc39b0d5816a0b76f6112
Joseph Choi <jc566@njit.edu>
2017-04-25 18:02:50 -0400

    added alwaysinfront of player script

---
+++ Assets/Scripts/AlwaysInfrontOfPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlwaysInfrontOfPlayer : MonoBehaviour {
+
+	public GameObject infrontPlayer;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = infrontPlayer.transform.position;
+	}
+}
