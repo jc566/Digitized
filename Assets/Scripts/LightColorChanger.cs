f500abca7a14ed4801c230eee7ac7b0f53c545c2
JosephChoi <jc566@njit.edu>
2017-05-15 18:15:12 -0400

    adding changing colors scripts and pointlights on waypoints

---
+++ Assets/Scripts/LightColorChanger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightColorChanger : MonoBehaviour {
+	public Light waypointLight;
+
+	// Use this for initialization
+	void Start () {
+		waypointLight = this.gameObject.GetComponentInChildren<Light> ();
+
+		InvokeRepeating ("changeColors", 1.0f, 0.2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//Debug.Log (randomColor);
+	}
+
+	void changeColors()
+	{
+		Color randomColor = new Color (Random.value, Random.value, .1f);
+		waypointLight.color = randomColor;
+		Debug.Log (waypointLight.color);
+	}
+}
